ae60745b3d7e03da8744e53716b92ae9a2152edf
Js-cook <jscook1001@gmail.com>
2026-02-04 22:27:15 -0500

    Rough draft for player and enemy state interfaces. Will probably need some additions as we implement functionality

---
+++ Interfaces/IPlayerState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public interface IPlayerState
+{
+    void ChangeDirection();
+    void BeDead();
+    void BeDamaged();
+    void BeAttacking();
+    void BeIdle();
+    void Update();
+    
+}
