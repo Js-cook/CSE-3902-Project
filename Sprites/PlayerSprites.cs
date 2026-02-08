d81db48af8d00c82d660b4aea90a7d50a5bcb0d3
Js-cook <jscook1001@gmail.com>
2026-02-08 18:10:20 -0500

    Added some rough sprite classes

---
+++ Sprites/PlayerSprites.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprites
+{
+    public class LeftMovingPlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class RightMovingPlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class UpMovingPlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class DownMovingPlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class LeftIdlePlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class RightIdlePlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class UpIdlePlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+
+    public class DownIdlePlayerSprite : ISprite
+    {
+        public void Update()
+        {
+
+        }
+
+        public void SpriteDraw()
+        {
+
+        }
+    }
+}
+
+
