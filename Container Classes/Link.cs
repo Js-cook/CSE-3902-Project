ff11a4190760b400b9cff7d4629224f9af012829
Js-cook <jscook1001@gmail.com>
2026-02-08 16:41:06 -0500

    Added sprite assets

--- Container Classes/Link.cs
+++ Container Classes/Link.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Interfaces;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -7,9 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 public class Link
 {
     private Vector2 position;
+    private ISprite sprite;
+    private IPlayerState playerState;
+
+    public Link()
+    {
+        playerState = new RightIdlePlayerState(this);
+    }
     // need a state
     // need an ipsrite
 
