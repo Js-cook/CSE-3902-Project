88e6d689aa44f53e0c46234e3e137b923a34f915
Js-cook <jscook1001@gmail.com>
2026-02-11 18:19:16 -0500

    Starting work on projectiles and arrow sprites

--- Container Classes/Projectile.cs
+++ Container Classes/Projectile.cs
@@ -4,6 +4,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Projectile
+public class Boomerang : IProjectile
 {
+    public Boomerang()
+    {
+    }
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class MagicBoomerang : IProjectile
+{
+    public MagicBoomerang()
+    {
+    }
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Arrow : IProjectile
+{
+    public Arrow()
+    {
+    }
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class SilverArrow : IProjectile
+{
+    public SilverArrow()
+    {
+    }
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Bomb : IProjectile
+{
+    public Bomb()
+    {
+    }
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Fireball : IProjectile
+{
+    public Fireball()
+    {
+    }
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update()
+    {
+        throw new NotImplementedException();
+    }
 }
--- Sprites/ProjectileSprites.cs
+++ Sprites/ProjectileSprites.cs
@@ -1,10 +1,187 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 
-public class ProjectileSprites
+public class ProjectileSpriteFactory
 {
+    private Texture2D projectileTexture;
+    private SpriteBatch spriteBatch;
 
+    public ProjectileSpriteFactory(Texture2D texture, SpriteBatch spriteBatch)
+    {
+        projectileTexture = texture;
+        this.spriteBatch = spriteBatch;
+    }
+
+    public ISprite CreateBoomerangSprite(Vector2 position)
+    {
+        return new BoomerangSprite(projectileTexture, position, spriteBatch);
+    }
+    public ISprite CreateMagicBoomerangSprite(Vector2 position)
+    {
+        return new MagicBoomerangSprite(projectileTexture, position, spriteBatch);
+    }
+    public ISprite CreateArrowSprite(Vector2 position, string direction)
+    {
+        return new ArrowSprite(projectileTexture, position, spriteBatch, direction);
+    }
+    public ISprite CreateSilverArrowSprite(Vector2 position, string direction)
+    {
+        return new SilverArrowSprite(projectileTexture, position, spriteBatch, direction);
+    }
+    public ISprite CreateBombSprite(Vector2 position)
+    {
+        return new BombSprite(projectileTexture, position, spriteBatch);
+    }
+    public ISprite CreateFireballSprite(Vector2 position)
+    {
+        return new FireballSprite(projectileTexture, position, spriteBatch);
+    }
+}
+
+public class BoomerangSprite : ISprite
+{
+    private Texture2D texture;
+    private SpriteBatch spriteBatch;
+    private Vector2 position;
+
+    private Rectangle sourceRectangle = new Rectangle(0, 0, 16, 16);
+
+    public BoomerangSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch)
+    {
+    }
+    public void SpriteDraw(Vector2 position)
+    {
+        throw new NotImplementedException();
+    }
+    public void Update(GameTime gametime)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class MagicBoomerangSprite : ISprite
+{
+    public MagicBoomerangSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch)
+    {
+    }
+    public void SpriteDraw(Vector2 position)
+    {
+        throw new NotImplementedException();
+    }
+    public void Update(GameTime gametime)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ArrowSprite : ISprite
+{
+    private Texture2D texture;
+    private SpriteBatch spriteBatch;
+    private Vector2 position;
+    private string direction;
+
+    private Rectangle sourceRectangleHorizontal = new Rectangle(9, 184, 17, 17);
+    private Rectangle sourceRectangleVertical = new Rectangle(0, 184, 9, 17);
+
+    public ArrowSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch, string direction)
+    {
+        this.texture = texture;
+        this.spriteBatch = spriteBatch;
+        this.position = position;
+        this.direction = direction;
+    }
+    public void SpriteDraw(Vector2 position)
+    {
+        switch(direction)
+        {
+            case "up":
+                spriteBatch.Draw(texture, position, sourceRectangleVertical, Color.White);
+                break;
+            case "down":
+                spriteBatch.Draw(texture, position, sourceRectangleVertical, Color.White, 0.0f, new Vector2(0,0), 1.0f, SpriteEffects.FlipVertically, 0.0f);
+                break;
+            case "left":
+                spriteBatch.Draw(texture, position, sourceRectangleHorizontal, Color.White, 0.0f, new Vector2(0,0), 1.0f, SpriteEffects.FlipHorizontally, 0.0f);
+                break;
+            case "right":
+                spriteBatch.Draw(texture, position, sourceRectangleHorizontal, Color.White);
+                break;
+        }
+    }
+    public void Update(GameTime gametime)
+    {
+        // arrow doesn't need to animate
+    }
+}
+
+public class SilverArrowSprite : ISprite
+{
+    private Texture2D texture;
+    private SpriteBatch spriteBatch;
+    private Vector2 position;
+    private string direction;
+
+    private Rectangle sourceRectangleHorizontal = new Rectangle(35, 184, 17, 17);
+    private Rectangle sourceRectangleVertical = new Rectangle(26, 184, 9, 17);
+    public SilverArrowSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch, string direction)
+    {
+        this.texture = texture;
+        this.spriteBatch = spriteBatch;
+        this.position = position;
+        this.direction = direction;
+    }
+    public void SpriteDraw(Vector2 position)
+    {
+        switch (direction)
+        {
+            case "up":
+                spriteBatch.Draw(texture, position, sourceRectangleVertical, Color.White);
+                break;
+            case "down":
+                spriteBatch.Draw(texture, position, sourceRectangleVertical, Color.White, 0.0f, new Vector2(0, 0), 1.0f, SpriteEffects.FlipVertically, 0.0f);
+                break;
+            case "left":
+                spriteBatch.Draw(texture, position, sourceRectangleHorizontal, Color.White, 0.0f, new Vector2(0, 0), 1.0f, SpriteEffects.FlipHorizontally, 0.0f);
+                break;
+            case "right":
+                spriteBatch.Draw(texture, position, sourceRectangleHorizontal, Color.White);
+                break;
+        }
+    }
+    public void Update(GameTime gametime)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class BombSprite : ISprite
+{
+    public BombSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch)
+    {
+    }
+    public void SpriteDraw(Vector2 position)
+    {
+        throw new NotImplementedException();
+    }
+    public void Update(GameTime gametime)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class FireballSprite : ISprite
+{
+    public FireballSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch)
+    {
+    }
+    public void SpriteDraw(Vector2 position)
+    {
+        throw new NotImplementedException();
+    }
+    public void Update(GameTime gametime)
+    {
+        throw new NotImplementedException();
+    }
 }
