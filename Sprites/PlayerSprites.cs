44480f44bd9f57fd3b274d050c9313f78f4d4190
Js-cook <jscook1001@gmail.com>
2026-02-11 13:33:42 -0500

    Started making up attacking player states/sprites

--- Sprites/PlayerSprites.cs
+++ Sprites/PlayerSprites.cs
@@ -421,4 +421,58 @@ namespace Sprites
             spriteBatch.Draw(texture, position, currentFrame, Color.White);
         }
     }
+
+    public class UpAttackingPlayerSprite : ISprite
+    {
+        private Texture2D texture;
+        private SpriteBatch spriteBatch;
+        private Rectangle currentFrame;
+
+        private double attackTimer = 0.0;
+        private double attackDuration = 0.4;
+
+        private Rectangle[] frameContainer =
+        {
+            new Rectangle(0, 76, 16, 17), // WRONG VALUES PLEASE CHANGE ME
+            new Rectangle(69, 76, 19, 17),
+            new Rectangle(45, 76, 23, 17),
+            new Rectangle(17, 76, 27, 17)
+        };
+
+        public UpAttackingPlayerSprite(Texture2D texture, Vector2 position, SpriteBatch spriteBatch)
+        {
+            this.texture = texture;
+            this.spriteBatch = spriteBatch;
+            currentFrame = frameContainer[0];
+        }
+        public void Update(GameTime gametime)
+        {
+            attackTimer += gametime.ElapsedGameTime.TotalSeconds;
+            if (attackTimer < 0.1)
+            {
+                currentFrame = frameContainer[0];
+            }
+            else if (attackTimer >= 0.1 && attackTimer < 0.2)
+            {
+                currentFrame = frameContainer[1];
+            }
+            else if (attackTimer >= 0.2 && attackTimer < 0.3)
+            {
+                currentFrame = frameContainer[2];
+            }
+            else if (attackTimer >= 0.3 && attackTimer < 0.4)
+            {
+                currentFrame = frameContainer[3];
+            }
+            else if (attackTimer >= 0.4)
+            {
+                currentFrame = frameContainer[0];
+                attackTimer = 0.0;
+            }
+        }
+        public void SpriteDraw(Vector2 position)
+        {
+            spriteBatch.Draw(texture, position, currentFrame, Color.White);
+        }
+    }
 }
