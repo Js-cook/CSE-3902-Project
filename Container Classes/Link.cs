418deef5248419b993c78a24ebb8b22f6ab3224c
Hashim <hashimbinzafar1234@gmail.com>
2026-02-11 13:36:17 -0500

    Started to work on Gel

--- Container Classes/Link.cs
+++ Container Classes/Link.cs
@@ -1,4 +1,4 @@
-﻿using Interfaces;
+﻿ using Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
---
+++ Sprites/EnemySprites/GelSprite.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna;
+using Sprites;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+
+public class GelSpriteFactory
+{
+    private Texture2D gelTexture;
+    private SpriteBatch spriteBatch;
+    public GelSpriteFactory(Texture2D gelTexture, SpriteBatch spriteBatch)
+    {
+        this.gelTexture = gelTexture;
+        this.spriteBatch = spriteBatch;
+    }
+
+    public ISprite CreateMovingGelSprite(Vector2 position)
+    {
+        return new MovingGelSprite(gelTexture, position, spriteBatch);
+    }
+
+
+    
+}
+
+
+
