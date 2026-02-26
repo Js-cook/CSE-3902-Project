d720bef6bf55c2292ca4868c03f2fcc2bb16ed7d
Js-cook <jscook1001@gmail.com>
2026-02-26 13:10:17 -0500

    Started work on level file reader

---
+++ Helper/LevelFileReader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+
+public class LevelFileReader
+{
+    private TileFactory tileFactory;
+    private List<ISprite> levelContent = new();
+
+    public LevelFileReader(TileFactory tileFactory)
+    {
+        this.tileFactory = tileFactory;
+    }
+
+    // possibly change out string for enum or something
+    public void LoadLevel(string filePath)
+    {
+
+    }
+
+}
