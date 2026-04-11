21f2a53eda2b3c5bfbd083312996337f44c3d312
Js-cook <jscook1001@gmail.com>
2026-04-11 16:33:35 -0400

    Implemented HUD inventory cursor

---
+++ Sprites/HUD/HUDItems/HUDCursorSprite.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HUDCursorSprite : ISprite
+{
+    private Texture2D texture;
+    private SpriteBatch spriteBatch;
+
+    private Rectangle cursor = new Rectangle(654, 108, 8, 8);
+
+    public HUDCursorSprite(Texture2D texture, SpriteBatch spriteBatch)
+    {
+        this.texture = texture;
+        this.spriteBatch = spriteBatch;
+    }
+
+    public void Update(GameTime gametime)
+    {
+    }
+
+    public void SpriteDraw(Vector2 position)
+    {
+        int scale = 6;
+
+        Rectangle destinationRectangle = new Rectangle(
+            (int)position.X,
+            (int)position.Y,
+            cursor.Width * scale,
+            cursor.Height * scale
+        );
+        spriteBatch.Draw(texture, destinationRectangle, cursor, Color.White);
+    }
+}
--- States/GameStates/InventoryState.cs
+++ States/GameStates/InventoryState.cs
@@ -18,10 +18,23 @@ public class InventoryState : IGameState
 
     private HUDSpriteFactory textFactory;
 
+    private int[] cursorXPositions = [523, 619, 715, 811];
+    private int[] cursorYPositions = [175, 232];
+
+    private int currentX = 0;
+    private int currentY = 0;
+
+    private HUDCursorSprite cursorSprite;
+    //private Vector2 cursorPosition = new Vector2(811, 232);
+
+    //private int horizontalShift = 25;
+    //private int verticalShift = 25;
+
+    private int buttonCooldown = 0;
+
     public InventoryState(LinkInventory inventory, SpriteBatch spriteBatch)
     {
         Signal = GameStateSignal.NONE;
-        //savedPlayingState = playingState;
         playerInventory = inventory;
         this.spriteBatch = spriteBatch;
     }
@@ -30,23 +43,63 @@ public class InventoryState : IGameState
         // Load inventory content here
         textFactory = new HUDSpriteFactory(contentLoader.Load<SpriteFont>("Fonts/the-legend-of-zelda-nes"), spriteBatch, contentLoader.Load<Texture2D>("HUD"), contentLoader.Load<Texture2D>("LinkSprites"));
         inventoryScreenSprite = new InventoryScreenSprite(contentLoader.Load<Texture2D>("HUD"), spriteBatch, textFactory, playerInventory);
+        cursorSprite = textFactory.CreateHUDCursorSprite(new Vector2(cursorXPositions[0], cursorXPositions[0]));
     }
     public void ResolveKey(KeyboardState keyState)
     {
+
         if (keyState.IsKeyDown(Keys.LeftControl))
         {
             Signal = GameStateSignal.TO_SAVED_PLAYING;
+        }
+
+        if(keyState.IsKeyDown(Keys.Left) && buttonCooldown == 0)
+        {
+            if(currentX == 0)
+            {
+                currentX = cursorXPositions.Length - 1;
+            } else
+            {
+                currentX--;
+            }
+            buttonCooldown = 20;
+        }
+        if(keyState.IsKeyDown(Keys.Right) && buttonCooldown == 0)
+        {
+            currentX = (currentX + 1) % cursorXPositions.Length;
+            buttonCooldown = 20;
+        }
+        if(keyState.IsKeyDown(Keys.Up) && buttonCooldown == 0)
+        {
+            if(currentY == 0)
+            {
+                currentY = 1;
+            } else
+            {
+                currentY--;
+            }
+            buttonCooldown = 20;
+        }
+        if(keyState.IsKeyDown(Keys.Down) && buttonCooldown == 0)
+        {
+            currentY = (currentY + 1) % cursorYPositions.Length;
+            buttonCooldown = 20;
         }
     }
     public void Update(GameTime gameTime)
     {
         // Update inventory state here
+        if (buttonCooldown > 0)
+        {
+            buttonCooldown--;
+        }
         inventoryScreenSprite.Update(gameTime);
     }
     public void Draw()
     {
         // Draw inventory UI here
         inventoryScreenSprite.SpriteDraw(Vector2.Zero);
+        cursorSprite.SpriteDraw(new Vector2(cursorXPositions[currentX], cursorYPositions[currentY]));
     }
     public void ResetState()
     {
