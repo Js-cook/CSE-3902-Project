b75e9d4541484b0d1c32cdb45f913e2321557039
Js-cook <jscook1001@gmail.com>
2026-04-13 17:45:30 -0400

    Room data should now be fetched from XML file

--- Helper/Rooms.cs
+++ Helper/Rooms.cs
@@ -1,6 +1,9 @@
 using Enums;
 using System;
+using System.Xml;
 using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 public class EnemyDefinition
 {
     public string Type { get; set; }
@@ -55,26 +58,89 @@ public static class RoomsRepository
 
     static RoomsRepository()
     {
-        _rooms = new Dictionary<(int, int), RoomDefinition>
+        _rooms = new Dictionary<(int, int), RoomDefinition>();
+
+        string filePath = Path.Combine("Content", "RoomData.xml");
+
+        Debug.WriteLine(filePath);
+
+        using (XmlReader reader = XmlReader.Create(filePath))
         {
-            { (0, 1), CreateRoom0_1() },
-            { (0, 2), CreateRoom0_2() },
-            { (1, 2), CreateRoom1_2() },
-            { (2, 2), CreateRoom2_2() },
-            { (2, 1), CreateRoom2_1() },
-            { (2, 0), CreateRoom2_0() },
-            { (3, 1), CreateRoom3_1() },
-            { (3, 2), CreateRoom3_2() },
-            { (4, 2), CreateRoom4_2() },
-            { (5, 1), CreateRoom5_1() },
-            { (5, 2), CreateRoom5_2() },
-            { (2, 3), CreateRoom2_3() },
-            { (3, 3), CreateRoom3_3() },
-            { (5, 3), CreateRoom5_3() },
-            { (1, 4), CreateRoom1_4() },
-            { (2, 4), CreateRoom2_4() },
-            { (1, 5), CreateRoom1_5() },
-        };
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "Room")
+                {
+                    int row = int.Parse(reader.GetAttribute("row"));
+                    int col = int.Parse(reader.GetAttribute("col"));
+
+                    string[][] tiles = null;
+                    var doors = new Dictionary<string, string>();
+                    var enemies = new List<EnemyDefinition>();
+
+                    using (XmlReader roomReader = reader.ReadSubtree())
+                    {
+                        while (roomReader.Read())
+                        {
+                            // -------- TILES --------
+                            if (roomReader.NodeType == XmlNodeType.Element && roomReader.Name == "Tiles")
+                            {
+                                var tileRows = new List<string[]>();
+
+                                using (XmlReader tileReader = roomReader.ReadSubtree())
+                                {
+                                    while (tileReader.Read())
+                                    {
+                                        if (tileReader.NodeType == XmlNodeType.Element && tileReader.Name == "row")
+                                        {
+                                            string rowData = tileReader.ReadElementContentAsString();
+                                            string[] tilesSplit = rowData.Split(',');
+                                            tileRows.Add(tilesSplit);
+                                        }
+                                    }
+                                }
+
+                                tiles = tileRows.ToArray();
+                            }
+
+                            // -------- DOORS --------
+                            if (roomReader.NodeType == XmlNodeType.Element && roomReader.Name == "Doors")
+                            {
+                                using (XmlReader doorReader = roomReader.ReadSubtree())
+                                {
+                                    while (doorReader.Read())
+                                    {
+                                        if (doorReader.NodeType == XmlNodeType.Element)
+                                        {
+                                            if (doorReader.Name == "Top" ||
+                                                doorReader.Name == "Right" ||
+                                                doorReader.Name == "Bottom" ||
+                                                doorReader.Name == "Left")
+                                            {
+                                                string type = doorReader.GetAttribute("type");
+                                                doors[doorReader.Name] = type;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            // -------- ENEMIES --------
+                            if (roomReader.NodeType == XmlNodeType.Element && roomReader.Name == "Enemy")
+                            {
+                                string type = roomReader.GetAttribute("type");
+                                int x = int.Parse(roomReader.GetAttribute("x"));
+                                int y = int.Parse(roomReader.GetAttribute("y"));
+
+                                enemies.Add(new EnemyDefinition(type, x, y));
+                            }
+                        }
+                    }
+
+                    var roomDef = new RoomDefinition(row, col, tiles, doors, enemies);
+                    _rooms[(row, col)] = roomDef;
+                }
+            }
+        }
     }
 
     public static RoomDefinition GetRoom(int row, int col)
